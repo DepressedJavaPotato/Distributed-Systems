0314550142f1281c623e38d4233b7aa3ff35be56
Maaz Arshad Beg <maazarshad.beg@student.adelaide.edu.au>
2024-08-10 19:54:53 +0930

    Base structure for try catch

--- CalculatorServer.java
+++ CalculatorServer.java
@@ -2,8 +2,11 @@
 public class CalculatorServer {
     public static void main(String[] args) {
         try {
+            //local host
+            System.err.println("Calculator server ready");
             
         } catch (Exception e) {
+            System.err.println("Calculator server failed with error: "+e);
         }
     }
 }

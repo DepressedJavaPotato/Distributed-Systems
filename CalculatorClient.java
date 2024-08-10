5988781a9b56ae4cffc33f0fc7ac97b282dbc333
Maaz Arshad Beg <maazarshad.beg@student.adelaide.edu.au>
2024-08-10 20:26:52 +0930

    Basic print to start structure for client

--- CalculatorClient.java
+++ CalculatorClient.java
@@ -1,9 +1,15 @@
 
+import java.rmi.Naming;
+
+
 public class CalculatorClient {
     public static void main(String[] args) {
         try {
-            
+            Calculator calculator = (Calculator) Naming.lookup("rmi://localhost/CalculatorService");
+            System.out.println("Is stack empty? " + calculator.isEmpty());
         } catch (Exception e) {
+
+            System.err.println("Calculator Client failed: " + e);
         }
     }
 }

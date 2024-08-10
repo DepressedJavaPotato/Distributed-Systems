2370c082359987f93a61866762e76344bde3dbdd
Maaz Arshad Beg <maazarshad.beg@student.adelaide.edu.au>
2024-08-10 19:56:21 +0930

    Updated implementation for server

--- CalculatorServer.java
+++ CalculatorServer.java
@@ -1,8 +1,14 @@
 
+import java.rmi.registry.LocateRegistry;
+
+
 public class CalculatorServer {
     public static void main(String[] args) {
         try {
             //local host
+            LocateRegistry.createRegistry(1099);
+            Calculator calculator = new CalculatorImplementation();
+            Naming.rebind("rmi://localhost/CalculatorService", calculator);
             System.err.println("Calculator server ready");
             
         } catch (Exception e) {

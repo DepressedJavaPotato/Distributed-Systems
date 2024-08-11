5fcfda7d0fbb3477d75b2c3f593d54fcc96fac3c
Maaz Arshad Beg <maazarshad.beg@student.adelaide.edu.au>
2024-08-11 21:32:53 +0930

    trying out max functionality

--- src/CalculatorClient.java
+++ src/CalculatorClient.java
@@ -6,10 +6,18 @@ public class CalculatorClient {
     public static void main(String[] args) {
         try {
             Calculator calculator = (Calculator) Naming.lookup("rmi://localhost/CalculatorService");
+
             calculator.pushValue(10);
             calculator.pushValue(20);
             calculator.pushOperation("min");
             System.out.println("Min: " + calculator.pop());
+            
+            calculator.pushValue(5);
+            calculator.pushValue(15);
+            calculator.pushOperation("gcd");
+            System.out.println("GCD: " + calculator.pop()); // Should print 5
+
+
             System.out.println("Is stack empty? " + calculator.isEmpty());
         } catch (Exception e) {
 
